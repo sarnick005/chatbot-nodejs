99c314a46372a3ed37e54a2cc6563f02b1568bb2
sarnick005 <sarnick.chakraborty@gmail.com>
2024-11-14 20:19:34 +0530

    fixed cors

--- src/app.js
+++ src/app.js
@@ -6,10 +6,12 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
+    origin:
+      process.env.CORS_ORIGIN || "https://chatbot-react-zd9z.onrender.com", 
     credentials: true,
   }),
 );
+
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
